9522a604ab18dd916a4509d333ce3c55ba2b1706
Andrew Hassan <andrewihassan@gmail.com>
2020-06-07 12:17:31 -0400

    Remove some beego HTML template stuff

--- controllers/default.go
+++ controllers/default.go
@@ -4,10 +4,12 @@ import (
 	"github.com/astaxie/beego"
 )
 
+// MainController is the root beego controller for this app
 type MainController struct {
 	beego.Controller
 }
 
+// Get is the GET request handler for MainController
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
