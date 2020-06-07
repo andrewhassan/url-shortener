22be1df09ed5c5f5ea6c4e4c880fd11d77951f66
Andrew Hassan <andrew.hassan@shopify.com>
2020-06-07 19:11:31 -0400

    Seed rand

--- utils/slug.go
+++ utils/slug.go
@@ -1,11 +1,15 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var chars = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
 // CreateSlug creates a random slug of length `length` using alphanumeric characters
 func CreateSlug(length int) string {
+	rand.Seed(time.Now().UnixNano())
 	result := make([]rune, length)
 	for i := range result {
 		result[i] = chars[rand.Intn(len(chars))]
